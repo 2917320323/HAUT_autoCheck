68a226185bf6e449771398786c8ce6660e93dfe4
2917320323 <57084821+2917320323@users.noreply.github.com>
2021-01-10 14:20:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def getUserJson(userInfo,token):
                 "text": userInfo['classDescription']
             },
             #如果你来自其他学校，请自行打卡抓包修改地址字段
-            "areaStr": {"streetNumber":"","street":"长椿路辅路","district":"中原区","city":"郑州市","province":"河南省","town":"","pois":"河南工业大学(莲花街校区)","lng":113.55064699999795 + random.random()/1000,"lat":34.83870696238093 + random.random()/1000,"address":"中原区长椿路辅路河南工业大学(莲花街校区)","text":"河南省-郑州市","code":""},
+            "areaStr": {"streetNumber":"128附9","street":"伊河路","district":"中原区","city":"郑州市","province":"河南省","town":"","pois":"伊河路17号院","lng":113.63189699999617 + random.random()/1000,"lat":34.74878097249976 + random.random()/1000,"address":"中原区伊河路128附9伊河路17号院","text":"河南省-郑州市","code":""},
             "reportdate": round(time.time()*1000),
             "customerid": userInfo['customerId'],
             "deptid": userInfo['classId'],
